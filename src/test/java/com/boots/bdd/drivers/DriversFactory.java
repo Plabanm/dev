bff78dcea4a3e0ed32d12323a3bc2deb13cba413
mahesh reddy <kodumurubhavana@gmail.com>
2018-06-15 16:01:20 +0100

    sample

--- src/test/java/com/boots/bdd/drivers/DriversFactory.java
+++ src/test/java/com/boots/bdd/drivers/DriversFactory.java
@@ -18,50 +18,59 @@ import java.util.concurrent.TimeUnit;
 
 public class DriversFactory {
 
-private String browser="";
-public static WebDriver driver;
+    private String browser = "";
+    public static WebDriver driver;
 
-public void openBrowser(){
-    switch (browser){
-        case "Chrome":
-        {
-            ChromeDriverManager.getInstance().setup();
-            driver= new ChromeDriver();
-            break;
-        }
-        case "ie":
-            {
-            InternetExplorerDriverManager.getInstance().setup();
-            driver= new InternetExplorerDriver();
-            break;
-        }
-        case "Opera":
-            {
-            OperaDriverManager.getInstance().setup();
-            driver= new OperaDriver();
-            break;
-        }
-        default:
-        {
-            FirefoxDriverManager.getInstance().setup();
-            driver = new FirefoxDriver();
-            break;
+    public void openBrowser() {
+        switch (browser) {
+            case "Chrome": {
+                ChromeDriverManager.getInstance().setup();
+                driver = new ChromeDriver();
+                break;
+            }
+            case "ie": {
+                InternetExplorerDriverManager.getInstance().setup();
+                driver = new InternetExplorerDriver();
+                break;
+            }
+            case "Opera": {
+                OperaDriverManager.getInstance().setup();
+                driver = new OperaDriver();
+                break;
+            }
+            default: {
+                FirefoxDriverManager.getInstance().setup();
+                driver = new FirefoxDriver();
+                break;
+            }
         }
     }
-}
 
-public void navigateUrl(String url){driver.get(url);}
-public void maximizeWindow(){driver.manage().window().maximize();}
-public void implWait(){driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);}
-public void closeBrowser(){driver.quit();}
-public void sleepBroswer(int ms){
-    try {
-        Thread.sleep(5000);
-    } catch (InterruptedException e) {
-        e.printStackTrace();
+    public void navigateUrl(String url) {
+        driver.get(url);
     }
-}
-public void embedScreenshot(Scenario scenario) {
+
+    public void maximizeWindow() {
+        driver.manage().window().maximize();
+    }
+
+    public void implWait() {
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+    }
+
+    public void closeBrowser() {
+        driver.quit();
+    }
+
+    public void sleepBroswer(int ms) {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void embedScreenshot(Scenario scenario) {
         try {
             byte[] screenShot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
             scenario.embed(screenShot, "image/png");
